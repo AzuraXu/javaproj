13f7973fa2f82ebea1aeaf32301436e5a13e9ee7
Azura Xu <122924124+AzuraXu@users.noreply.github.com>
2025-12-06 23:47:29 -0500

    Bugfixing and main program flow WIP

--- exercise/src/main/java/com/azuraxu/exercise/Manager.java
+++ exercise/src/main/java/com/azuraxu/exercise/Manager.java
@@ -6,10 +6,11 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.io.File;
-import java.io.Reader;
 import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.List;
 
 
 class exerciseItem{
@@ -47,6 +48,7 @@ public class Manager {
         Gson gson = new Gson();
         exerciseItem[] rawExList = new exerciseItem[1];
         foodItem[] rawFoodList = new foodItem[1];
+        boolean created;
 
         //On load - database imports
             try (
@@ -84,27 +86,50 @@ public class Manager {
         // --------- end database load
         
         try{
-            saveWriter = new FileWriter("savedata.txt");    
+            created = saveData.createNewFile();
+            saveWriter = new FileWriter("savedata.txt", true);    
             saveReader = new FileReader("savedata.txt");    
         }
         catch (IOException e) {
-      System.out.println("An error occurred while initializing save reading / writing");
+      System.out.println("An error occurred while initializing save file reading / writing");
         throw new RuntimeException(e);
     }
-        try {
-        if(saveData.createNewFile()) {
+        if(created){
             initialSetup(); }
         else {
             System.out.println("Welcome back!");
         }
-          } catch (IOException e) {
-      System.out.println("An error occurred while creating new save data");
-        throw new RuntimeException(e);
+
+
+    while (true){
+
+        System.out.println("Welcome! Choose what to do next from the following options:\n");
+        System.out.println("1: Edit starting data.");
+        System.out.println("2: Add new item");
+        System.out.println("3: Edit existing item");
+        // System.out.println("4: Edit starting data.");
+        switch(readInt("Enter an integer to proceed.")) {
+            case 1:
+
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            
+            default:
+            System.out.println("Invalid input (outside of range). Enter only the number that cooresponds to one listed above. Try again:");
+
+
+        }
+
+
     }
 
     }
 
     public static void initialSetup(){
+        try {saveWriter.write("200\n2000"); // placeholder values
         int weight = readInt("Enter your weight (in lbs):");
         int targetWeight = readInt("What would you like your final weight to be? (also in lbs): ");
         editWeight(weight);
@@ -112,16 +137,28 @@ public class Manager {
         int finalGoal = 2300 + (targetWeight - weight * 7700) / (7 * weeks);
         System.out.println("Your final calculated daily weight goal is: " + finalGoal +" calories.");
         System.out.println("You may edit this at any time.");
-        editGoal(finalGoal);
+        editGoal(finalGoal);}
+        catch (IOException e) {
+      System.out.println("An error occurred during initial setup");
+        throw new RuntimeException(e);
+    }
 
     }
 
-    public static void editGoal(int x){
-
+    public static void editGoal(int x) throws IOException {
+        String[] current = readFileToArray();
+        current[0] = String.valueOf(x);
+        try (FileWriter overwriter = new FileWriter("savedata.txt")) {
+            overwriter.write(current[0] + "\n" + current[1]);
+        }
     }
 
-    public static void editWeight(int x){
-
+    public static void editWeight(int x) throws IOException {
+        String[] current = readFileToArray();
+        current[1] = String.valueOf(x);
+        try (FileWriter overwriter = new FileWriter("savedata.txt")) {
+            overwriter.write(current[0] + "\n" + current[1]);
+        }
     }
     public static int readInt(String prompt){
         System.out.println(prompt);
@@ -137,5 +174,10 @@ public class Manager {
     }          
         }
     }
-    
+    public static String[] readFileToArray() throws IOException {
+        List<String> lines = Files.readAllLines(Path.of("savedata.txt"));
+        System.err.println(lines);
+    return lines.toArray(new String[0]);
+}
+
 }
